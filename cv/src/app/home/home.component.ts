e08dbdf0ef569de2b58338305db19b26214e269b
Ilies Ait Hamouda <ilies.aithamouda@bnc.ca>
2019-03-17 00:18:36 -0400

    add picture and hi message typed.js

--- cv/src/app/home/home.component.ts
+++ cv/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typed from 'typed.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    // Can also be included with a regular script tag
+    var options = {
+      strings: ["I'm a human being", "I'm a Husband","I'm a Father","I'm a Dreamer..","Oh and i'm a Developer :)"],
+      loop: false,
+      backSpeed: 1,
+      typeSpeed: 100,
+      smartBackspace: true,
+      startDelay: 0,
+    }
+    var typed = new Typed(".introduction-phrase", options);
+    
   }
 
 }
