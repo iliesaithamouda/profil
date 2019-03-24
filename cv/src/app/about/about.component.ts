ad4ff96a7f0a4fa6051b90edc220f9cb84334ca3
Ilies Ait Hamouda <ilies.aithamouda@bnc.ca>
2019-03-24 12:34:14 -0400

    services and work done

--- cv/src/app/about/about.component.ts
+++ cv/src/app/about/about.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  expertises: Array<{ domain: string; percentage: number }>
+  constructor() {
+    this.expertises = new Array<{ domain: string; percentage: number }>();
+    this.expertises.push({ domain: 'Angular', percentage: 75});
+    this.expertises.push({ domain: 'Adobe experience manager', percentage: 60});
+    this.expertises.push({ domain: 'Java', percentage: 50});
+    this.expertises.push({ domain: 'React', percentage: 10});
+  }
 
   ngOnInit() {
   }
