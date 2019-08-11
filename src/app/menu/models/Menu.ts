f002e58a9b2561d1625ef60ea51cf3d6c04f6c02
iliesaithamouda <iliesaithamouda@gmail.com>
2019-08-10 22:09:53 -0400

    add some other changes

--- src/app/menu/models/Menu.ts
+++ src/app/menu/models/Menu.ts
@@ -6,7 +6,7 @@ export class Menu {
         this.items.push({label: 'Home', link: '/home', selected: true});
         this.items.push({label: 'About', link: '/about', selected: false});
         this.items.push({label: 'Services', link: '/services', selected: false});
-        this.items.push({label: 'Work', link: '/work', selected: false});
+        //this.items.push({label: 'Work', link: '/work', selected: false});
         this.items.push({label: 'Posts', link: '/posts', selected: false});
         this.items.push({label: 'Contact', link: '/contact', selected: false});
     }
--- src/app/portfolio/portfolio.service.ts
+++ src/app/portfolio/portfolio.service.ts
@@ -23,34 +23,34 @@ export class PortfolioService {
   getServicesItems(): Array<{imgPath: string; title: string; description: string}> {
       return [
         {
-          imgPath : 'fa-angular', 
+          imgPath : 'fab fa-angular', 
           title: 'ANGULAR', 
           description: 'Learning this language from the start was a pleasure. To be able to see how far it got is exquisite'
         },
         {
-          imgPath : 'fa-adobe', 
+          imgPath : 'fab fa-adobe', 
           title: 'AEM & Cold Fusion', 
           description: 'slowly but surely, I\'m trying to speak the adobe language, so rich and yet not used that much'
         },
         {
-          imgPath : 'fa-java', 
+          imgPath : 'fab fa-java', 
           title: 'JAVA', 
           description: 'What a pleasure to write in Java, my first language and still the one'
         },
         {
-          imgPath : 'fa-html5', 
+          imgPath : 'fab fa-html5', 
           title: 'HTML', 
           description: 'Althought i spoke so many languages through the years, HTML is a classic that we\'ll never get rid of'
         },
         {
-          imgPath : 'fa-node-js', 
+          imgPath : 'fab fa-node-js', 
           title: 'Nodejs', 
           description: 'I can\'t dream of a better way for a front-end developer to do a back-end job'
         },
         {
-          imgPath : 'fa-react', 
-          title: 'react', 
-          description: 'I\'m still a beginner, but a fast learner. I can\'t simply not speak the most spoken reactive front-end language'
+          imgPath : 'fas fa-infinity', 
+          title: 'DevOps', 
+          description: 'Jenkins, UCD, Ansible, Dockers, GCP, AWS, Azure,  what else ? so many buzzwords, but they have no secret for me'
         }
       ];
   }
