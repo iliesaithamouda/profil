7971b464092e4f0c97cabb187ae6a9d604d1afaa
iliesaithamouda <iliesaithamouda@gmail.com>
2019-11-24 21:46:50 -0500

    update project

--- src/app/_deprecated_onscroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnscrollDirective } from './_deprecated_onscroll.directive';
-
-describe('OnscrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnscrollDirective(null, null);
-    expect(directive).toBeTruthy();
-  });
-});
